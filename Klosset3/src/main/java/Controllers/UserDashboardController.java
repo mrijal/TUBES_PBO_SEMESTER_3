df3562a9a7e5cbc2b1cb2b1ee5391818c80a29f6
anifbur <jamananip@gmail.com>
2025-02-02 12:47:52 +0700

    ganti lagi
    hehe

--- Klosset3/src/main/java/Controllers/UserDashboardController.java
+++ Klosset3/src/main/java/Controllers/UserDashboardController.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Parent;
 
 public class UserDashboardController {
@@ -21,23 +23,31 @@ public class UserDashboardController {
     @FXML private TableColumn<Asset, String> colName;
     @FXML private TableColumn<Asset, String> colCategory;
     @FXML private TableColumn<Asset, String> colStatus;
-    @FXML private TableColumn<Asset, String> colDate;
+    @FXML private TableColumn<Asset, String> colcreated_at;
     @FXML private Button logoutButton;
+    @FXML private TextField search;
 
-    @FXML
+    
     public void initialize() {
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
         colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        colcreated_at.setCellValueFactory(new PropertyValueFactory<>("created_at"));
         loadAssets();
+        
+        search.textProperty().addListener(new ChangeListener<String>() {
+          
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                search(newValue);
+            }
+        });
     }
 
     public void loadAssets() {
         assetsTable.getItems().clear();
         try (Connection connection = DatabaseConnection.connect()) {
-            String query = "SELECT * FROM assets WHERE created_by = ?";
+            String query = "SELECT * FROM assets";
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, 1); // Ganti dengan ID user yang login
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 assetsTable.getItems().add(new Asset(
@@ -99,6 +109,29 @@ public class UserDashboardController {
 
             stage.setTitle("Ajukan Aset");
             stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void search(String keyword) {
+        assetsTable.getItems().clear();
+        try (Connection connection = DatabaseConnection.connect()) {
+            String query = "SELECT * FROM assets WHERE name LIKE ? OR category LIKE ? OR status LIKE ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, "%" + keyword + "%");
+            statement.setString(2, "%" + keyword + "%");
+            statement.setString(3, "%" + keyword + "%");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                assetsTable.getItems().add(new Asset(
+                        resultSet.getInt("id_asset"),
+                        resultSet.getString("name"),
+                        resultSet.getString("category"),
+                        resultSet.getString("description"),
+                        resultSet.getString("status"),
+                        resultSet.getString("created_at")
+                ));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
